8ea2747e60061dcd1a30553c307e46cc49b72333
Ting-Chih Hsiao <nail.hsiao@acer.com>
2016-01-18 16:03:11 +0800

    add comment for floppy_init()
    Signed-off-by: Ting-Chih Hsiao <nail.hsiao@acer.com>

--- linux-0.11/kernel/blk_drv/floppy.c
+++ linux-0.11/kernel/blk_drv/floppy.c
@@ -458,6 +458,8 @@ void do_fd_request(void)
 void floppy_init(void)
 {
 	blk_dev[MAJOR_NR].request_fn = DEVICE_REQUEST;
-	set_trap_gate(0x26,&floppy_interrupt);
-	outb(inb_p(0x21)&~0x40,0x21);
+
+	/* enable floppy interrupt */
+	set_trap_gate(0x26, &floppy_interrupt);
+	outb(inb_p(0x21) & 0xbf, 0x21);
 }
